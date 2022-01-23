3ebef990915f47aeb160d53ddb37680f2a980682
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-01-23 18:44:25 +0530

    API developed giving all the data of restaurants for now

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
@@ -9,16 +10,18 @@ const port = process.env.PORT || 8000
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize:50,
-        wtimeout:2500,
-        useNewUrlParse: true
+        maxPoolSize:50,
+        wtimeoutMS:2500,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     }
 )
 .catch(err => {
     console.error(err.stack)
     process.exit(1)
 })
-.then(async Client => {
+.then(async client => {
+    await RestaurantsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listening on port ${port}`)
     })
