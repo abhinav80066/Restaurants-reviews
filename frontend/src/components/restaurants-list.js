bf1c7147ebffb3329f5e1b48a571edd92c231eca
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-01-24 13:22:07 +0530

    Setup the router and components

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Restaurantslist() {
+  return  (
+    <div>
+        restaurants list
+    </div>
+    )
+}
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Restaurants() {
+    return (
+    <div>
+        Hello world
+    </div>
+    )}
